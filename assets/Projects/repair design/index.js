0d2db67f53047ee391a04d0fae1916c73795b843
romanrg <nekhaichyk.raman@gmail.com>
2019-10-20 19:29:48 +0300

    feat: add repeir design project

---
+++ assets/Projects/repair design/index.js
@@ -0,0 +1,14 @@
+
+const mobile = document.getElementsByClassName('wrapper__change--mobile')[0];
+const desktop = document.getElementsByClassName('wrapper__change--desktop')[0];
+const representation = document.getElementsByClassName('wrapper')[0];
+mobile.addEventListener('click', ()=> {
+    representation.classList.add('wrapper__mobile');
+    representation.classList.remove('wrapper__desktop');  
+})
+
+desktop.addEventListener('click', ()=> {
+    representation.classList.add('wrapper__desktop');
+    representation.classList.remove('wrapper__mobile');  
+})
+
