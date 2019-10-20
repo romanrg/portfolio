98224d8294f353dd0fdc843a055f4981d0295800
romanrg <nekhaichyk.raman@gmail.com>
2019-10-20 19:44:22 +0300

    feat: add the_yallow project

--- assets/Projects/repair design/index.js
+++ assets/Projects/repair design/index.js
@@ -5,10 +5,10 @@ const representation = document.getElementsByClassName('wrapper')[0];
 mobile.addEventListener('click', ()=> {
     representation.classList.add('wrapper__mobile');
     representation.classList.remove('wrapper__desktop');  
-})
+});
 
 desktop.addEventListener('click', ()=> {
     representation.classList.add('wrapper__desktop');
     representation.classList.remove('wrapper__mobile');  
-})
+});
 
